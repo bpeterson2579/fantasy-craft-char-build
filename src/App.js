8336aa97012c7ff59a5a429e22cdaab80583563a
Brian Peterson <bpeterson1579@gmail.com>
2021-12-20 11:03:58 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import Ideas from './Ideas';
+import Form from './Form';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ideas: [],
+    }
+  }
+
+  addIdea = (newIdea) => {
+    this.setState({ideas: [...this.state.ideas, newIdea]});
+  }
+
+  deleteIdea = (id) => {
+    console.log(id);
+    const filteredIdeas = this.state.ideas.filter(idea => idea.id != id);
+
+    this.setState({ideas: filteredIdeas});
+  }
+
+  render() {
+    return (
+      <main className='App'>
+        <h1>Idea Box</h1>
+        {!this.state.ideas.length && <h2>No ideas yet -- add some!</h2>}
+        <Form addIdea={this.addIdea} />
+        <Ideas ideas={this.state.ideas} deleteIdea={this.deleteIdea}/>
+      </main>
+    )
+  }
+}
+
+export default App;
